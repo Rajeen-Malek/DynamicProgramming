dca2523e5cfa49633416af4a9d42e90324da6536
Rajeen-Malek <48065908+Rajeen-Malek@users.noreply.github.com>
2021-01-06 21:38:58 +0530

    Update Gridtraveller.cpp

--- Gridtraveller.cpp
+++ Gridtraveller.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int ma[999][999] ;
+int ma[999][999] ;    //Initialized the global matrix to store the preceding values
 int grid ( int n , int m  )
 {
     if(n==1 and m==1) return 1;
     if(n==0 || m == 0) return 0;
-    if(ma[n][m]!=-1) return ma[n][m];
+    if(ma[n][m]!=-1) return ma[n][m];   // checking if the value is present in the matrix
     
     ma[n][m] = (grid (n-1,m) + grid(n,m-1));
 
